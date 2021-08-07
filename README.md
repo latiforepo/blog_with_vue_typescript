1bf65d5e117dba2fd9b333adb61a31ab91f90715
Abdul Latif <85255258+latiforepo@users.noreply.github.com>
2021-08-07 13:25:05 +0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,28 @@
+# blog
+
+## Blog features
+```
+Dynamic content, Json Server, HashTag, Sorting, Post Creation, Composition API, Vue 3
+```
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+### Run Json Server for dynamic data
+```
+npx json-server --watch data/db.json
+```
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
---
+++ src/router/index.ts
@@ -0,0 +1,44 @@
+import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
+import Home from '../views/Home.vue'
+import Details from '@/views/Details.vue'
+import Create from '@/views/Create.vue'
+import Tag from '@/views/Tag.vue'
+
+const routes: Array<RouteRecordRaw> = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/posts/:id',
+    name: 'Details',
+    component: Details,
+    props: true
+  },
+  {
+    path: '/create',
+    name: 'Create',
+    component: Create
+  },
+  {
+    path: '/tags/:tag',
+    name: 'Tag',
+    component: Tag
+  },
+  {
+    path: '/about',
+    name: 'About',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
